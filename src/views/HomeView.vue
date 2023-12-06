ae8a05cd3637295c06ea204f95a6ff3969057c4b
A H Shadhin <shadhin.riseuplabs@gmail.com>
2023-12-06 13:11:57 +0600

    import validator composable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,29 +6,49 @@
                     <div class="card">
                         <div class="card-header">Form</div>
                         <div class="card-body">
-                            <form @submit.prevent="submit" method="post" autocomplete="off">
+                            <form @submit.prevent="submit"
+                                method="post"
+                                autocomplete="off">
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" v-model="data.name" id="name" class="form-control">
+                                    <label for="name"
+                                        class="form-label">Name</label>
+                                    <input type="text"
+                                        v-model="data.name"
+                                        id="name"
+                                        class="form-control">
                                     <div class="text-danger">{{ validator.firstError('name') }}</div>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">E-Mail</label>
-                                    <input type="text" v-model="data.email" id="email" class="form-control">
+                                    <label for="email"
+                                        class="form-label">E-Mail</label>
+                                    <input type="text"
+                                        v-model="data.email"
+                                        id="email"
+                                        class="form-control">
                                     <div class="text-danger">{{ validator.firstError('email') }}</div>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="mobile" class="form-label">Mobile</label>
-                                    <input type="text" v-model="data.mobile" id="mobile" class="form-control">
+                                    <label for="mobile"
+                                        class="form-label">Mobile</label>
+                                    <input type="text"
+                                        v-model="data.mobile"
+                                        id="mobile"
+                                        class="form-control">
                                     <div class="text-danger">{{ validator.firstError('mobile') }}</div>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="age" class="form-label">Age</label>
-                                    <input type="text" v-model.number="data.extra.age" id="age" class="form-control">
+                                    <label for="age"
+                                        class="form-label">Age</label>
+                                    <input type="text"
+                                        v-model.number="data.extra.age"
+                                        id="age"
+                                        class="form-control">
                                     <div class="text-danger">{{ validator.firstError('extra.age') }}</div>
                                 </div>
                                 <div class="mb-2">
-                                    <input type="submit" value="Submit" class="btn btn-primary">
+                                    <input type="submit"
+                                        value="Submit"
+                                        class="btn btn-primary">
                                 </div>
                             </form>
                         </div>
@@ -41,7 +61,7 @@
 
 <script setup>
 import { reactive } from 'vue';
-import Validator from '../plugins/validator';
+import { useValidator } from '../composables/validator';
 
 const data = reactive({
     name: '',
@@ -52,7 +72,7 @@ const data = reactive({
     }
 });
 
-const validator = new Validator(data, {
+const validator = useValidator(data, {
     name: 'required|between:3,5',
     // email: 'required|email',
     // 'extra.age': 'nullable|between:3,5',
